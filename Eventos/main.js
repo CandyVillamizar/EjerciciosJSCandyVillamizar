aab9938410721113fc6cf20f9d5684a6ceab0ace
candy villamizar <you@example.com>
2024-07-14 22:45:32 -0500

    nuevos cambios

---
+++ Eventos/main.js
@@ -0,0 +1,101 @@
+
+let boton = document.getElementById("submit");
+
+boton.addEventListener("click", a =>{
+    document.getElementById("resultado").value = (document.getElementById("peso").value) / 
+    ((document.getElementById("estatura").value*document.getElementById("estatura").value*0.0001))
+    console.log(resultado.value)
+})
+
+let dolar = document.getElementById("dolar")
+let peso = document.getElementById("pesos")
+
+dolar.addEventListener('keyup', a=>{
+     peso.value = a.target.value * 3800
+})
+
+peso.addEventListener('keyup', a=>{
+    dolar.value = a.target.value / 3800
+})
+
+notas = [
+    nota1={
+        id: 1,
+        titulo: "Sacar la basura",
+        texto: "mi mama me va a retar si no lo hago",
+        realizada: false
+    },
+    nota2={
+        id:2,
+        titulo:"Hacer la tarea",
+        texto: "Es para el lunes",
+        realizada: false
+    },
+    nota3={
+        id:3,
+        titulo:"Hacer ejercicio",
+        texto: "Todos los dias",
+        realizada:false
+    },
+    nota4={
+        id:4,
+        titulo:"Hacer la cena",
+        texto: "Todos los dias",
+        realizada:false
+    }
+]
+
+let idGLobal=notas[3].id
+console.log(idGLobal);
+for (let i= 0; i<notas.length; i++) {
+    
+    function pintarTarjetas(notas) {
+        let contenedor = document.getElementById("Notas")
+        let tarjeta = document.createElement('div')
+        tarjeta.className="card"
+        tarjeta.innerHTML = `
+        <div ${notas[i].id} class = "card">
+        </div>
+        <div class="card-body">
+          <input class="form-check-input" onClick="(${notas[i].id})" type="checkbox">
+          <h4 class="card-title">${notas[i].titulo}</h4>
+          <p class="card-text">${notas[i].texto}.</p>
+        </div>`
+        contenedor.appendChild(tarjeta)
+      }
+      pintarTarjetas(notas)
+      
+  }
+  
+guardar.addEventListener("click",(agregarNota) =>{
+    notas.push({
+        id: idGLobal+1,
+        titulo: document.getElementById("titulo").value,
+        texto: document.getElementById("textonota").value,
+        realizada: false
+    })
+    
+    document.getElementById("titulo").value=""
+    document.getElementById("textonota").value=""
+    idGLobal=notas[notas.length-1].id
+
+    espacioNota=document.createElement("div")
+    espacioNota.id=`nota${notas[notas.length-1].id}`
+    espacioNota.className=`card p-2 m-2`
+    espacioNota.innerHTML=`
+    <div ${notas[notas.length-1].id} class = "card">
+        </div>
+        <div class="card-body">
+          <h4 class="card-title">${notas[notas.length-1].titulo}</h4>
+          <p class="card-text">${notas[notas.length-1].texto}.</p>
+        </div>
+     `
+    console.log(notas);
+    pintarTarjetas(notas)
+}   
+)
+
+
+
+
+
