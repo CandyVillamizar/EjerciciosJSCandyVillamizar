e5d78614ca6d345c3cd485a197796369aabe1f46
candy villamizar <you@example.com>
2024-07-16 22:35:31 -0500

    ultimos cambios

--- Eventos/main.js
+++ Eventos/main.js
@@ -1,4 +1,4 @@
-
+/*
 let boton = document.getElementById("submit");
 
 boton.addEventListener("click", a =>{
@@ -17,7 +17,10 @@ dolar.addEventListener('keyup', a=>{
 peso.addEventListener('keyup', a=>{
     dolar.value = a.target.value / 3800
 })
-
+*/
+//Ejercicio 3. Realizar una aplicación de notas (el front queda a gusto de cada uno).
+//a. Crear un array donde vamos a guardar las notas
+//b. Agregar un par de notas de prueba como la siguiente
 notas = [
     nota1={
         id: 1,
@@ -45,72 +48,119 @@ notas = [
     }
 ]
 
+//c. Crear una variable idGlobal e inicializala en el mismo valor del ultimo id que creaste manualmente, usaremos esto como control de las notas.
 let idGLobal=notas[3].id
 console.log(idGLobal);
+
+//d. Crear un div que va a ser el contenedor de las notas.
+//e. Crear una función que pinte las notas en forma de tarjetas dentro del div contenedor.
 for (let i= 0; i<notas.length; i++) {
     
     function pintarTarjetas(notas) {
-        let contenedor = document.getElementById("Notas")
+        let contenedor1 = document.getElementById("Notas")
         let tarjeta = document.createElement('div')
         tarjeta.className="card"
         tarjeta.innerHTML = `
-        <div ${notas[i].id} class = "card">
-        </div>
-        <div class="card-body">
-          <input class="form-check-input" onClick="(${notas[i].id})" type="checkbox">
+        <div ${notas[i].id} class="card-body">
+          <input onClick="marcarRealizada(${notas[i].id})" type="checkbox">
           <h4 class="card-title">${notas[i].titulo}</h4>
           <p class="card-text">${notas[i].texto}.</p>
-          <button id="borrar" type="text" class="btn btn-primary">Borrar</button>
+          <button id="borrarNota" onClick="borrarNota(${notas[i].id}) type="text" class="btn btn-primary">Borrar</button>
         </div>
         `
-        contenedor.appendChild(tarjeta)
+        contenedor1.appendChild(tarjeta)
       }
       pintarTarjetas(notas)
   }
-  
-guardar.addEventListener("click",(agregarNota) =>{
+//f. Sobre el div contenedor, crear una pequeña interfaz para crear nuevas notas:Un input de texto para el titulo, un textarea para el texto y 2 botones, uno para 
+//guardar la nota nueva y otro para limpiar los campos.
+
+//g. Crear una función agregarNota la cual necesitara 2 parametros: titulo y texto. La cual deberá crear un objeto de tipo nota como en el punto b 
+//y agregarlo al array de notas.
+
+//h. Al presionar el botón guardar deberá guardar en variables los valores de los inputs y verificar si no están vacíos, en cuyo caso deberá llamar a la función que agregara 
+//la nueva nota y paso seguido volver a pintar las notas en la pantalla.
+
+guardar.addEventListener("click",() =>{
     notas.push({
         id: idGLobal++,
         titulo: document.getElementById("titulo").value,
         texto: document.getElementById("textonota").value,
         realizada: false
-    })   
-        document.getElementById("titulo").value=""
-        document.getElementById("textonota").value=""
-        idGLobal=notas[notas.length-1].id
-    
-        let contenedor = document.getElementById("Notas")
-        let tarjeta = document.createElement('div')
-        tarjeta.className="card"
-        tarjeta.innerHTML = `
-        <div ${notas[notas.length-1].id} class = "card">
-        </div>
-        <div class="card-body">
-          <input class="form-check-input" onClick="(${notas[notas.length-1].id})" type="checkbox">
-          <h4 class="card-title">${notas[notas.length-1].titulo}</h4>
-          <p class="card-text">${notas[notas.length-1].texto}.</p>
-          <button id="borrar" type="text" class="btn btn-primary">Borrar</button>
-        </div>
-        `
-        contenedor.appendChild(tarjeta)
-        console.log(notas);
-        pintarTarjetas(notas)   
-})
+    }), 
+    document.getElementById("titulo").value=""
+    document.getElementById("textonota").value=""
+    idGLobal=notas[notas.length-1].id
 
+    let contenedor = document.getElementById("Notas")
+    let tarjeta = document.createElement('div')
+    tarjeta.className="card"
+    tarjeta.innerHTML = `
+    <div ${notas[notas.length-1].id} class = "card">
+    </div>
+    <div class="card-body">
+      <input onClick="marcarRealizada(${notas[notas.length-1].id})" type="checkbox" ${notas.realizada? "checked": ""}>
+      <h4 class="card-title">${notas[notas.length-1].titulo}</h4>
+      <p class="card-text">${notas[notas.length-1].texto}.</p>
+      <button id="borrarNota" onClick="borrarNota() type="text" class="btn btn-primary">Borrar</button>
+    </div>
+    `
+    contenedor.appendChild(tarjeta)
+    console.log(notas);
+})
+//Agregar en el template de la tarjeta en la función que pinta un botón con texto “borrar nota” para borrar la nota, 
+//para este caso usaremos la propiedad onClick de la etiqueta button y dentro de ella llamaremos a una función que crearemos 
+//llamada borrarNota() que recibirá como parámetro el id de la misma. borrarNota(${elemento.id}).
+
+
+//Crearemos la función borrarNota la cual necesitara el parámetro id. La misma deberá eliminar el elemento cuyo id sea igual al recibido por parámetro 
+//y volver a pintar las notas para ver reflejado el cambio.
+
+function borrarNota(id) {
+    const elemento = document.getElementById();
+    if (elemento==id) { 
+      elemento.remove();
+    }
+    pintarTarjetas(notas);
+  }
+
+//Agregar una validación en la función que pinta las tarjetas, la cual deberá mostrar un mensaje dentro del div contenedor que diga NO HAY NOTAS PARA MOSTRAR 
+//en caso de no haber elementos en el array.
+
+function pintarTarjetas(notas) {
+    const contenedorNotas = document.getElementById("Notas"); 
+    contenedorNotas.innerHTML = ''; 
+    if (notas.length === 0) {
+      const mensaje = document.createElement("p");
+      mensaje.textContent = "NO HAY NOTAS PARA MOSTRAR";
+      contenedorNotas.appendChild(mensaje); 
+    } else {
+      notas.forEach(nota => {
+      });
+    }
+  }
+   
+//Al hacer click en el botón borrar que esta junto con el botón guardar se deberán limpiar los campos de titulo y de texto.
+  document.getElementById("borrar").addEventListener("click", function() {
+    document.getElementById("titulo").value = "";
+    document.getElementById("textonota").value = "";
+  });
+//
+
+//Crear la función marcarRealizada la cual recibirá como parámetro un id y deberá buscar el elemento dentro del array y cambiar la propiedad realizada 
+//por el valor contrario al que ya posee y volver a pintar los elementos en pantalla para verlo reflejado.
 function marcarRealizada(id) {
     for (let i = 0; i< notas.length; i++) {
-        if (notas[i].id==id) {
-            if (notas[i].realizada) {
-                notas[i].realizada == false
+        if (notas[i].id===id) {
+            if (notas[i].realizada==true) {
+                notas[i].realizada == false;
             }else{
-                notas[i].realizada == true
+                notas[i].realizada == true;
             }
         }
     }
+    
 }
 
-console.log(notas[1]);
 
-
-
-
+console.log(notas); 
